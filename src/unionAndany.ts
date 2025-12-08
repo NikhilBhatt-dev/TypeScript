b993a4d444a2ebfe025fcfa31818d1853738977e
Nikhil Bhatt <bhattnikhil158@gmail.com>
2025-12-08 18:21:05 +0530

    union&any

--- src/unionAndany.ts
+++ src/unionAndany.ts
@@ -2,4 +2,19 @@ let Subs : number | string = '10M'
 
 let apiRequestStauts : 'pending' |  'succees' | 'error' = "pending"
 
-apiRequestStauts = "nikhil"
+let airlineSeat : "aisle" |  "window" |  "middle" = 'aisle'
+ 
+
+const orders = ['12' , '33', ' 28', '43']
+
+let currentorder:string | undefined;
+
+for(let order of orders){
+    if(order === '28') {
+        currentorder =  order;
+        break;
+    }
+    currentorder = '11'
+}
+
+console.log(currentorder)
